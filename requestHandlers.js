e0adcda20775693b6e76a45d49357274b7678cc5
Alejandro <alejandro.lopez@spotbros.com>
2013-03-18 12:38:07 +0100

    no message

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,25 @@
-function getProfilePic(response) 
-{
+function streamFile(file,resp,contentType) {
+    path.exists(file, function(exists) {
+		if (exists) {
+			fs.readFile(file, function(err,data) {
+				if (err) {
+					resp.writeHead(500);
+					resp.end();
+					log(WARN, "No such file: ", file);
+				} else {
+					resp.writeHead(200, { 'Content-Type': contentType });
+					resp.end(data, 'utf-8');
+				}
+			});
+		} else {
+			resp.writeHead(404);
+			resp.end();
+		}
+	});
+}
+
+
+function getProfilePic(response) {
   response.writeHead(200, {"Content-Type": "text/html"}); //text/plain
   response.write("getProfilePic");
   response.end();
